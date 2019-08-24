25eca3ca4953ca737bb5e19fd10f63b02f3a708b
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2019-08-24 17:02:23 +0900

    ㅓㅗ

---
+++ app/src/main/java/com/example/phonecall/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.phonecall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
