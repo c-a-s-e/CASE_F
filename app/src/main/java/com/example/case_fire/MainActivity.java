2666ce76e116c3cab7e51cf5142956bf61a25df5
QueenCurry <yeawonjo@gmail.com>
2019-08-25 14:43:41 +0900

    mainActivity UI 수정

--- app/src/main/java/com/example/case_fire/MainActivity.java
+++ app/src/main/java/com/example/case_fire/MainActivity.java
@@ -27,23 +27,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         FirebaseMessaging.getInstance().subscribeToTopic("all");
 
-        Button rate_Button = (Button) findViewById(R.id.check_rate);
-        rate_Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), RateActivity.class);
-                view.getContext().startActivity(intent);}
-        });
-
-        Button reward_Button = (Button) findViewById(R.id.check_reward);
-        reward_Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), RewardActivity.class);
-                view.getContext().startActivity(intent);}
-        });
+//        Button rate_Button = (Button) findViewById(R.id.check_rate);
+//        rate_Button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(view.getContext(), RateActivity.class);
+//                view.getContext().startActivity(intent);}
+//        });
+//
+//        Button reward_Button = (Button) findViewById(R.id.check_reward);
+//        reward_Button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(view.getContext(), RewardActivity.class);
+//                view.getContext().startActivity(intent);}
+//        });
     }
 }
